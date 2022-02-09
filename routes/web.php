6349d0c99a67fc23551e66ce1f2aaea31c024b77
sawmu <sawmukahbaw191@gmail.com>
2022-02-09 21:42:23 +0800

    Email Confirm

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ use App\Http\Controllers\DbController;
 |
 */
 
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
 Route::get('/', function () {
     return view('welcome');
 });
