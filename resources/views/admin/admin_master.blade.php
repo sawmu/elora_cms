3229489817bcd4036ee5ff170243f46cfd718834
Saw <saw@firstcom.com.sg>
2022-02-11 10:04:04 +0800

    Footer

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -734,8 +734,8 @@
             <footer class="footer mt-auto">
                 <div class="copyright bg-white">
                     <p>
-                        &copy; <span id="copy-year">2019</span> Copyright Sleek Dashboard Bootstrap Template by
-                        <a class="text-primary" href="http://www.iamabdus.com/" target="_blank">Abdus</a
+                        &copy; <span id="copy-year">2019</span> Copyright by
+                        <a class="text-primary" href="http://www.mukahbaw.com/" target="_blank">Elora Co,. Ltd.</a
                 >.
               </p>
             </div>
