602cd963c029b1225e7152c55595cb4bc6b905e1
Saw <saw@firstcom.com.sg>
2022-02-18 16:59:39 +0800

    Merge branch 'master' of https://github.com/sawmu/elora_cms

--- tests/cypress/integration/auth.spec.js
+++ tests/cypress/integration/auth.spec.js
@@ -1,16 +1,21 @@
-// describe('Authentication', () => {
-//     it('signs a user in', () => {
-//         cy.refreshDatabase();
+describe('Authentication', () => {
+    it('signs a user in', () => {
+        // cy.refreshDatabase();
 
-//         cy.visit('/login');
+        cy.visit('/login');
+
+        
 
 
 
-//         cy.get('#email1')
-//             .type('fake@email.com')
-//             .should('have.value', 'fake@email.com')
-//     })
-// });
+        cy.get('#email')
+            .type('sawmukahbaw191@gmail.com');
+        cy.get('#password')
+            .type('152433Edin@');
+        
+        cy.contains('button', 'Log In').click();
+    })
+});
 
 // describe('My First Test', () => {
 //     it('Visits the Kitchen Sink', () => {
@@ -26,63 +31,3 @@
 //     })
 // })
 
-describe('Authenticaation', () => {
-
-    before(() => {
-        // runs once before all tests in the block
-    })
-
-    beforeEach(() => {
-        // runs before each test in the block
-        cy.visit('/')
-    })
-
-    afterEach(() => {
-        // runs after each test in the block
-    })
-
-    after(() => {
-        // runs once after all tests in the block
-    })
-
-
-    it('will redirect to login page when visitors accessing home', () => {
-        cy.visit('/')
-
-        cy.url().should('contains', '/login')
-    })
-
-    it('will register a user', () => {
-        cy.visit('/register')
-
-        cy.get('input[name=name]').type('admin')
-        cy.get('input[name=email]').type('user' + new Date().valueOf() + '@mail.com')
-        cy.get('input[name=password]').type('password')
-            // cy.get('input[name=password_confirmation]').type('password')
-
-        cy.get('button').contains('Register').click()
-
-        cy.url().should('contain', '/home')
-    })
-
-    it('will log in a user', () => {
-        cy.create('User').then(user => {
-            cy.visit('/login');
-
-            cy.get('input[name="email"]').type(user.email);
-            cy.get('input[name="password"]').type('password');
-            cy.get('button[type="submit"]').click();
-
-            cy.contains(user.name);
-            cy.contains('You are logged in!');
-        });
-    })
-
-    it('maintains user session', () => {
-        cy.login();
-
-        cy.visit('/home');
-
-        cy.contains('You are logged in!');
-    })
-})
